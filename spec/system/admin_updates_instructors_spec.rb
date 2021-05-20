a1f2e1dc38e947b59e1e2023ff8880e918eef16d
Laryssa_LFA <lfadarkangel@gmail.com>
2021-05-20 01:27:45 -0300

    Homework final

--- spec/system/admin_updates_instructors_spec.rb
+++ spec/system/admin_updates_instructors_spec.rb
@@ -15,7 +15,7 @@ describe 'Admin updates instructors' do
     expect(page).to have_text('Maria')
     expect(page).to have_text('maria@email.com')
     expect(page).to have_text('Formada em Ciências da Computação e leciona há 10 anos.')
-    expect(page).to have_text('img[src*="Maria.png"]')
+    expect(page).to have_css('img[src*="Maria.png"]')
     expect(page).to have_text('Professor(a) atualizado(a) com sucesso!')
     expect(page).to have_text('Voltar')
   end
