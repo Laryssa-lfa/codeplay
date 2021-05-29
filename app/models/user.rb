c47f05fa92123a2725565ab156ed791c96cbb4c2
Laryssa_LFA <lfadarkangel@gmail.com>
2021-05-28 23:28:28 -0300

    Implementação da Autenticação

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
+
+# TODO: traduzir as views do devise colocando no I18n
---
+++ spec/system/account_management_spec.rb
@@ -0,0 +1,96 @@
+require 'rails_helper'
+
+describe 'Account Management' do
+  context 'registration' do
+    it 'with email and password' do
+      visit root_path
+      click_on 'Registrar-me'
+      fill_in 'Email', with: 'jane@email.com'
+      fill_in 'Senha', with: '123456'
+      fill_in 'Confirmação de Senha', with: '123456'
+      click_on 'Criar conta'
+
+      expect(page).to have_text('Login efetuado com sucesso')
+      expect(page).to have_text('jane@email.com')
+      expect(current_path).to eq(root_path)
+      expect(page).to_not have_link('Registrar-me')
+      expect(page).to have_link('Sair')
+    end
+
+    it 'without valid field' do
+      visit root_path
+      click_on 'Registrar-me'
+      click_on 'Criar conta'
+
+      expect(page).to have_text('não pode ficar em branco', count: 2)
+      expect(page).to have_link('Entrar')
+    end
+
+    it 'password not match confirmation' do
+      visit root_path
+      click_on 'Registrar-me'
+      fill_in 'Email', with: 'jane@email.com'
+      fill_in 'Senha', with: '123456'
+      fill_in 'Confirmação de Senha', with: '123457'
+      click_on 'Criar conta'
+
+      expect(page).to have_text('Confirmação de Senha não é igual a Senha')
+      expect(page).to have_link('Entrar')
+    end
+
+    it 'with email not unique' do
+      User.create!(email: 'jane@email.com', password: '123456')
+
+      visit root_path
+      click_on 'Registrar-me'
+      fill_in 'Email', with: 'jane@email.com'
+      fill_in 'Senha', with: '123456'
+      fill_in 'Confirmação de Senha', with: '123456'
+      click_on 'Criar conta'
+
+      expect(page).to have_text('Email já está em uso')
+    end
+  end
+
+  context 'sign in' do
+    it 'with email and password' do
+      User.create!(email: 'jane@email.com', password: '123456')
+
+      visit root_path
+      click_on 'Entrar'
+      fill_in 'Email', with: 'jane@email.com'
+      fill_in 'Senha', with: '123456'
+      within 'form' do
+        click_on 'Entrar'
+      end
+
+      expect(page).to have_text('Login efetuado com sucesso')
+      expect(page).to have_text('jane@email.com')
+      expect(current_path).to eq(root_path)
+      expect(page).to_not have_link('Registrar-me')
+      expect(page).to_not have_link('Entrar')
+      expect(page).to have_link('Sair')
+    end
+
+    it 'without valid field' do
+      
+    end
+  end
+
+  context 'logout' do
+    it 'successfully' do
+      user = User.create!(email: 'jane@email.com', password: '123456')
+
+      login_as user, scope: :user
+      visit root_path
+      click_on 'Sair'
+
+      expect(page).to have_text('Saiu com sucesso')
+      expect(page).to_not have_text('jane@email.com')
+      expect(current_path).to eq(root_path)
+      expect(page).to have_link('Registrar-me')
+      expect(page).to have_link('Entrar')
+      expect(page).to_not have_link('Sair')
+    end
+  end
+end
